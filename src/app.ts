2d83262b8108255c3dfc48ae31b0dcad509bfc04
Francisca <fraan928@gmail.com>
2021-12-08 20:51:31 -0300

    Code to refactor

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 import express from "express";
 import cors from "cors";
 
+import * as recommendationController from "./controllers/recommendationController";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/test", (req, res) => {
-  res.send("OK!");
-});
+app.post("/recommendation", recommendationController.postRecommendation);
+app.post(
+  "/recommendations/:id/upvote",
+  recommendationController.upvoteRecommendation
+);
+app.post(
+  "/recommendations/:id/downvote",
+  recommendationController.downvoteRecommendation
+);
+
+app.get(
+  "/recommendations/random",
+  recommendationController.getRandomRecommendations
+);
 
 export default app;
---
+++ src/repositories/recommendationRepository.ts
@@ -0,0 +1,68 @@
+import connection from "../database";
+
+export async function create(name: string, youtubeLink: string, score: number) {
+  await connection.query(
+    `
+    INSERT INTO recommendations
+    (name, "youtubeLink", score)
+    VALUES
+    ($1, $2, $3)
+  `,
+    [name, youtubeLink, score]
+  );
+}
+
+export async function findById(id: number) {
+  const result = await connection.query(
+    `
+    SELECT * FROM recommendations WHERE id = $1
+  `,
+    [id]
+  );
+
+  return result.rows[0];
+}
+
+export async function incrementScore(id: number, increment: number) {
+  return await connection.query(
+    `
+    UPDATE recommendations SET score = score + $1 WHERE id = $2
+  `,
+    [increment, id]
+  );
+}
+
+export async function destroy(id: number) {
+  return await connection.query(
+    `
+    DELETE FROM recommendations WHERE id = $1
+  `,
+    [id]
+  );
+}
+
+export async function findRecommendations(
+  minScore: number,
+  maxScore: number = Infinity,
+  orderBy: string = ""
+) {
+  let where = "";
+  let params = [minScore];
+
+  if (maxScore === Infinity) {
+    where = "score >= $1";
+  } else {
+    where = "score BETWEEN $1 AND $2";
+    params.push(maxScore);
+  }
+
+  let query = `SELECT * FROM recommendations WHERE ${where}`;
+
+  if (orderBy) {
+    query += ` ORDER BY ${orderBy}`;
+  }
+
+  const result = await connection.query(query, params);
+
+  return result.rows;
+}
